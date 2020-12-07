b5c17986aaeeafaf7b58e01d19e239f506e2c38f
Midhet Sulemani <midhetfatema94@gmail.com>
2020-12-07 13:34:05 +0300

    first commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Waveline Media on 12/7/20.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
